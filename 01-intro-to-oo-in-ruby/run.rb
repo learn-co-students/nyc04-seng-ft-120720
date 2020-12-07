b75bc1fd7e2a8faa760fd8a89bf3f464faff552d
sylwiavargas <sylwia.vargas@gmail.com>
2020-12-07 18:02:59 -0500

    updates run.rb

--- 01-intro-to-oo-in-ruby/run.rb
+++ 01-intro-to-oo-in-ruby/run.rb
@@ -1,24 +1,19 @@
 require 'pry'
 
-class String
-
-    def excited
-        self.upcase + "!!!"
-    end
-
-end
-
-require 'pry'
-
+# let's create some dogs
 dog1 = {"name": "ruby", "breed": "maltese"}
-dog2 = {"name": "Gus", "breed": "Bouvier"}
+dog2 = {"namne": "Gus", "breed": "Bouvier"}
 dog3 = {"name": "Finn", "breed": "Cavapoo"}
+# this is too repetitive ^ 
+# let's create blueprint for Ruby to mass-produce some Dog instances
+# dry = don't repeat yourself
 
+# here's how we define classes:
 class Dog
 end
 
 class Animal
-
+# this method does not YET work. it will tomorrow.
     def to_s
         puts "hi, I'm a wild beast rawwwr"
     end
@@ -28,13 +23,10 @@ class Animal
     end
 end
 
-simba = Animal.new
+simba = Animal.new #this instance is saved to a variable
+Animal.new #this is also an instance! 
 simba.sleep 
 
 binding.pry
 0
 
-#  Define object in programming domain
-#  Create a class and instantiate an instance of the class
-
-# dry = don't repeat yourself
