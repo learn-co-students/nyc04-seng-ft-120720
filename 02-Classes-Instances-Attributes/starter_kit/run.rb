cdab677f21840fdce30152709490c5bdc68ef187
sylwiavargas <sylwia.vargas@gmail.com>
2020-12-08 10:22:42 -0500

    before lecture

---
+++ 02-Classes-Instances-Attributes/starter_kit/run.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Dog
+    
+    def bark
+        puts "woof"
+    end
+
+end
+
+dog1 = {"name": "ruby", "breed": "maltese"}
+dog2 = {"name": "Gus", "breed": "Bouvier"}
+dog3 = {"name": "Finn", "breed": "Cavapoo"}
+
+
+
+binding.pry
+0
