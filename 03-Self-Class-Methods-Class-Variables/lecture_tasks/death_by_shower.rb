edcad59afc2d8549c1b7050147287b12584a8d7c
sylwiavargas <sylwia.vargas@gmail.com>
2020-12-09 15:56:07 -0500

    adds the joke

---
+++ 03-Self-Class-Methods-Class-Variables/lecture_tasks/death_by_shower.rb
@@ -0,0 +1,92 @@
+require 'pry'
+
+class Person
+
+    attr_reader :name
+    attr_accessor :last_hunger_change, :hunger_level, :hunger_time
+
+    def initialize(name, hunger_time = 0.1)
+        @name = name
+        @last_hunger_change = Time.now
+        @hunger_level = 5.0
+        #how fast gets hungry (in sec)
+        @hunger_time = 0.1
+        @@all << self
+    end
+
+    #this is a class variable, you can update it from any point in the class
+    @@bubbles = "bubbles everywhere"
+
+    # this array will hold all the instances
+    @@all = []
+
+    def shower
+        head_n_shoulders
+    end
+
+    #the label of "Head'n'Shoulders" reads: rinse, lather, repeat
+    def head_n_shoulders
+        puts "😩😩😩😩😩😩😩😩😩😩 getting hungry: #{hunger_level} 😩😩😩😩😩😩😩😩😩😩😩😩"
+        #check if the person should already get hungry
+        if self.check_hunger
+            #if hunger_level is still ok, just go on with the showering
+            if hunger_level >= 1
+                rinse
+                lather
+                head_n_shoulders #repeat, meaning: call itself
+            #die from hunger
+            else
+                puts "💀 💀 💀 DEAD 💀 💀 💀 "
+                return "#{@name} no more"
+            end
+        else
+        #if not hungry yet, just rinse, lather, repeat
+            rinse
+            lather
+            head_n_shoulders #repeat, meaning: call itself
+        end
+    end
+
+    def rinse
+        puts "💧 💧 💧 💧 💧 💧 💧  rinses 💧 💧 💧 💧 💧💧  "
+    end
+
+    def lather
+        # will output the class variable @@bubbles
+        puts @@bubbles
+        # will add "bubbles" to the beginning
+        @@bubbles = "bubbles 🧼, " + @@bubbles
+    end
+
+    def hungry_yet?
+        #check if 0,3s. has passed since last hunger change -- this will result in a boolean (true or false)
+        Time.now - last_hunger_change >= hunger_time
+    end
+
+    def check_hunger 
+        if hungry_yet? #if check_hunger method returns true
+            #if it's true, the hunger_level drops
+            # !!!! THIS NEEDS TO BE AN INSTANCE VAR IF YOU WANT TO USE "-=" operator (method cannot -= from itself)
+            @hunger_level -= hunger_time
+            #and we update the last hunger change
+            last_hunger_change = Time.now
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
+end
+
+programmer = Person.new("Programmer")
+
+# this code is an illustration of the joke:
+# How did a programmer die in the shower?
+# They read the shampoo bottle: rinse, lather, repeat.
+
+#run this:
+# programmer.shower
+
+binding.pry
+0
--- 03-Self-Class-Methods-Class-Variables/lecture_tasks/person-sylwia.rb
+++ 03-Self-Class-Methods-Class-Variables/lecture_tasks/person-sylwia.rb
@@ -1,6 +1,58 @@
-reruire 'pry'
-# DO NOT CODE IN THIS FILE, SYLWIA WILL USE IT IN THE LECTURE
+require 'pry'
 
+class Person
+
+    # 2. The person should be instantiated with these attributes: `last_hunger_level_change`, `name`, `hunger_level`, `hunger_time`.
+    
+# 2a. When creating a new person, we should provide only an argument of a name and we **could** provide an argument of a hunger_time but not necessarily;
+
+# 2b. `hunger_time` should default to 0.1 unless the user passed another value. Moreover, each person should be initialized with this data:
+# `last_hunger_level_change` - current time (find a Ruby method that provides that)
+#  `hunger_level` - 3.0
+
+# 2c. write `attr_` macros for each of the attributes; follow your own judgement but also keep in mind that: 
+# - name will remain unchanged in this class but we will want to read it;
+# - we won't be reading `hunger_time` and `last_hunger_level_change` but we will be changing it. 
+
+    attr_reader :name
+    attr_accessor :hunger_time, :hunger_level, :last_hunger_level_change
+
+    def initialize(name_par, hunger_time_par = 0.1)
+        @name = name_par
+        @hunger_time = hunger_time_par
+        @hunger_level = 3.0
+        @last_hunger_level_change = Time.now
+    end
+
+    # 3. Add an instance behavior `Person#shower`. Its body can be empty, return whatever you want or just puts something.
+
+    def shower
+        puts "ocean ocean ocean"
+        head_n_shoulders
+    end
+
+    def head_n_shoulders
+        rinse
+        lather 
+        head_n_shoulders
+        # repeat
+    end
+
+    def rinse
+        puts "💧 💧 💧 💧 💧 💧 💧  rinses 💧 💧 💧 💧 💧💧  "
+    end
+
+    def lather
+        puts "🧼 🧼 🧼 🧼"
+    end
+
+    # def repeat
+    #     head_n_shoulders
+    # end
+
+end
+
+programmer = Person.new("programmer")
 
 binding.pry
 0
@@ -39,3 +91,7 @@ binding.pry
 # 10. 💯 Add a class method that returns an array of names of the instances called `names_only`
 
 # 11. 💯  Test whether you are getting an array of all names when you call `Person.names_only`.
+
+
+# How did a programmer die in the shower?
+# They read the shampoo bottle: rinse, lather, repeat.
