f99e0604b44f7317d43efd82decd449e3f839426
sylwiavargas <sylwia.vargas@gmail.com>
2020-12-09 13:06:49 -0500

    adds files

---
+++ 03-Self-Class-Methods-Class-Variables/lecture_tasks/person.rb
@@ -0,0 +1,32 @@
+reruire 'pry'
+
+
+binding.pry
+0
+
+
+# 1. Write a Person class.
+
+# 2. The person should be instantiated with these attributes: `last_hunger_level_change`, `name`, `hunger_level`, `hunger_time`. Only name will remain unchanged in this example but we will want to read it. We won't be reading `hunger_time` and `last_hunger_level_change` but we will be changing it. `hunger_time` should default to 0.1 unless the user passed another value. Moreover, each person should be initialized with this data:
+# `last_hunger_level_change` - current time (find a Ruby method that provides that)
+#  `hunger_level` - 3.0
+
+# 3. When creating a new person, we should provide only an argument of a name and we **could** provide an argument of a hunger_time but not necessarily;
+
+# 4. Test if you can create a person and if this person can be instantiated with/out the argument of `hunger_time` (hint: create two instances)
+
+# 5. Test if you can read all the attributes you are supposed to read and change all the attributes you are supposed to change; check if it errors out when you do the opposite;
+
+# 6. Add a class variable of "bubbles" (`@@bubbles`). The variable should hold a value of a string: "bubbles everywhere".
+
+# 7. Add a class variable of "all" (`@@all`) that hold an empty array.
+
+# 8. Add an instance behavior `Person#shower`. Its body can be empty, return whatever you want or just puts something.
+
+# 9. Add a class behavior `Person.all`, which returns all the instances of a person we have created. Remember to make sure that every new instance created is pushed into the `@@all` array.
+
+# Test whether you are getting an array of all instances when you call `Person.all`.
+
+# Add a class method that returns an array of names of the instances called `names_only`
+
+# Test whether you are getting an array of all names when you call `Person.names_only`.
---
+++ 03-Self-Class-Methods-Class-Variables/starter_kit/run.rb
@@ -0,0 +1,44 @@
+require 'pry'
+
+class Dog
+
+    attr_reader :breed
+    attr_accessor :fav_snack, :name, :attitude
+
+    def initialize(name_argument, fav_snack_argument, breed_argument, attitude_argument = "jolly")
+        @name = name_argument
+        @fav_snack = fav_snack_argument
+        @breed  = breed_argument
+        @number_of_legs = 4
+        @attitude = attitude_argument
+    end
+
+    def run
+        puts "#{@name} the #{@breed}running fast as the wind"
+    end
+
+    def sit
+        puts "#{@name} is sitting on its #{@number_of_legs} legs"
+    end
+
+    def bark(barking_sound)
+        puts "#{@name} says: '#{barking_sound.upcase}!'"
+    end
+
+    def sleep
+        puts "#{@name} is sleeping: 'zzzzz' and dreaming about #{@fav_snack}"
+    end
+
+end 
+
+ruby = Dog.new("ruby", "bacon", "maltese", "nihilistic")
+gus = Dog.new("Gus", "scooby snacks", "Bouvier")
+finn = Dog.new("Finn", "calm bites", "Cavapoo")
+
+binding.pry
+0
+
+ruby.run
+gus.sit
+finn.sleep
+ruby.bark
