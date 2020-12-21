c08943ea89d228e58bf758675adedc8853972ad5
sylwiavargas <sylwia.vargas@gmail.com>
2020-12-21 12:13:54 -0500

    before lecture

--- 07-AR-relationships/starter_kit/db/migrate/20201217165939_create_plants.rb
+++ 07-AR-relationships/starter_kit/db/migrate/20201217165939_create_plants.rb
@@ -2,9 +2,8 @@ class CreatePlants < ActiveRecord::Migration[5.2]
   def change
     create_table :plants do |table_helper|
       table_helper.string :species
-      table_helper.integer :height
       table_helper.string :color
-      t.datetime :bought
+      table_helper.datetime :bought
     end
   end
 end
