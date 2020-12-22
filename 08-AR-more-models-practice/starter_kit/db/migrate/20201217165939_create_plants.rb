9909f97397c53d8b0e13640e8b554db4da3e9ff3
sylwiavargas <sylwia.vargas@gmail.com>
2020-12-22 09:14:02 -0500

    deletes starter

--- 08-AR-more-models-practice/starter_kit/db/migrate/20201217165939_create_plants.rb
+++
@@ -1,9 +0,0 @@
-class CreatePlants < ActiveRecord::Migration[5.2]
-  def change
-    create_table :plants do |table_helper|
-      table_helper.string :species
-      table_helper.string :color
-      table_helper.datetime :bought
-    end
-  end
-end
