8e7791f96cbbff324c8f73d5450f05a5a6fe833e
Michelle Rios <brendamichelle995@gmail.com>
2021-02-03 11:25:33 -0500

    before lecture

---
+++ 25-arrow-functions/README.md
@@ -0,0 +1,10 @@
+# Arrow Functions
+
+1. Convert a regular function into an arrow function
+2. Understand how to use implicit returning
+3. Understand how to implicitly return an object
+4. Determine best use cases
+5. Use map, filter, and sort with an arrow function as a callback
+
+
+- [MDN Docs on Arrow Functions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
---
+++ 25-arrow-functions/index.js
@@ -0,0 +1,59 @@
+function doubleIt(num) {
+    return num * 2
+}
+
+
+
+
+
+const nums = [4, 66, 9, 77, 90, 10, 33, 1, 23]
+/********************** MAP **********************/
+// Transforms an array
+// Does not mutate the original array
+// Callback should return the transformation
+
+
+
+
+/********************** FILTER **********************/
+// Returns a subarray satisfying a condition
+// Does not mutate the original array
+// Callback should return a boolean
+
+
+
+const books = [
+    {
+        title: "We",
+        author: "Zamyatin",
+        yearRead: 2015
+    },
+    {
+        title: "Omon Ra",
+        author: "Pelevin",
+        yearRead: 2017
+    },
+    {
+        title: "Notes From Underground",
+        author: "Dostoevsky",
+        yearRead: 2019
+    },
+    {
+        title: "The Defense",
+        author: "Nabakov",
+        yearRead: 2014
+    },
+    {
+        title: "War and Peace",
+        author: "Tolstoy",
+        yearRead: 2016
+    }
+]
+const bookTitles = ["We", "Omon Ra", "The Defense", "Pnin", "War and Peace", "The Foundation Pit", "Notes From Underground"]
+/********************** SORT **********************/
+// mutates the original array
+// callback should return a number
+
+
+
+
