a6c41176e1a15cb4653ff8da66283b83de80c8ec
Michelle Rios <brendamichelle995@gmail.com>
2021-02-03 17:05:00 -0500

    after lecture

--- 25-arrow-functions/index.js
+++ 25-arrow-functions/index.js
@@ -1,6 +1,7 @@
-function doubleIt(num) {
-    return num * 2
-}
+// const doubleIt = num => num * 2
+
+// const createPerson = (name, age) => ({firstName: name, age: age})
+
 
 
 
@@ -12,6 +13,8 @@ const nums = [4, 66, 9, 77, 90, 10, 33, 1, 23]
 // Does not mutate the original array
 // Callback should return the transformation
 
+const newArr = nums.map(num => num * 2)
+
 
 
 
@@ -19,6 +22,8 @@ const nums = [4, 66, 9, 77, 90, 10, 33, 1, 23]
 // Returns a subarray satisfying a condition
 // Does not mutate the original array
 // Callback should return a boolean
+
+const filteredArr = nums.filter(num => num % 2 === 0)
 
 
 
@@ -47,12 +52,44 @@ const books = [
         title: "War and Peace",
         author: "Tolstoy",
         yearRead: 2016
+    },
+    {
+        title: "Anna Karenina",
+        author: "Tolstoy",
+        yearRead: 2016
     }
 ]
 const bookTitles = ["We", "Omon Ra", "The Defense", "Pnin", "War and Peace", "The Foundation Pit", "Notes From Underground"]
 /********************** SORT **********************/
 // mutates the original array
 // callback should return a number
+
+// least to greatest
+// nums.sort((num1, num2) => {
+//     return num1 - num2
+// })
+
+// greatest to least
+// nums.sort((num1, num2) => {
+//     return num2 - num1
+// })
+
+
+// SORT ON STRINGS
+// A - Z
+// bookTitles.sort((strA, strB) => {
+//     return strA.localeCompare(strB)
+// })
+
+// Z - A
+// bookTitles.sort((strA, strB) => {
+//     return strB.localeCompare(strA)
+// })
+
+// ON OBJECTS
+books.sort((bookA, bookB) => bookA.author.localeCompare(bookB.author))
+
+
 
 
 
