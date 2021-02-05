c8db1c1386ce222de4872a1cc259a810ccc0b330
Michelle Rios <brendamichelle995@gmail.com>
2021-02-05 10:46:13 -0500

    after lecture

--- 27-async-js-and-fetch/index.js
+++ 27-async-js-and-fetch/index.js
@@ -1,16 +1,43 @@
-function sleep(time) {
-    const start = new Date()
-    while (new Date() - start < time * 1000) {
-        // do nothing & block the main thread 
-    }
+// function sleep(time) {
+//     const start = new Date()
+//     while (new Date() - start < time * 1000) {
+//         // do nothing & block the main thread 
+//     }
+// }
+
+// sleep(5)
+// console.log('before timeout')
+
+// setTimeout(() => {
+//     console.log('inside timeout')
+// }, 5000)
+
+// console.log('after timeout')
+
+// response = RestClient.get('https://randomfox.ca/floof/')
+// data = JSON.parse(response.body)
+// puts data["image"]
+
+// fetch('https://randomfox.ca/floof/')
+//     .then(response => response.json())
+//     .then(foxObj => console.log(foxObj))
+
+let globalVar;
+
+
+const button = document.querySelector('#get-fox-btn')
+button.addEventListener('click', () => {
+    fetch('https://randomfox.ca/floof/')
+        .then(response => response.json())
+        .then(foxObj => {
+            displayFox(foxObj)
+        })
+})
+
+function displayFox(foxObj) {
+    const img = document.querySelector('img')
+    img.src = foxObj.image
+    globalVar = foxObj
 }
 
-document.querySelector('button').addEventListener('click', () => {
-    console.log("CLICKED !!!")
-})
-// translated ruby code from synch.rb
-console.log("Hello World!")
-console.log("brb, gonna go sleep for a sec")
-// sleep(5)
-
-console.log("What a nice cat nap that was!")
+// console.log(globalVar) // undefined
--- 27-async-js-and-fetch/synch.rb
+++ 27-async-js-and-fetch/synch.rb
@@ -1,12 +1,10 @@
-# require 'rest-client'
-# require 'json'
+require 'rest-client'
+require 'json'
 # require 'pry'
 # https://randomfox.ca/
 # https://randomfox.ca/floof/
 
 
-puts "Hello World!"
-puts "brb, gonna go sleep for a sec"
-sleep(5)
-
-puts "What a nice cat nap that was!"
+response = RestClient.get('https://randomfox.ca/floof/')
+data = JSON.parse(response.body)
+puts data["image"]
