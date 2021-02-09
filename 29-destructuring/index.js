7d2bace3bec8f20284480e66f01e6b96d9f27f84
Michelle Rios <brendamichelle995@gmail.com>
2021-02-09 16:19:55 -0500

    after lecture

--- 29-destructuring/index.js
+++ 29-destructuring/index.js
@@ -23,6 +23,8 @@ const raffy = {
 // const color = raffy.fruits.apple.color
 
 // new way
+// const raffles = undefined;
+const { name, age, type, bestFriends, fruits: { apple, orange }, beef = 44 } = raffy
 
 
 /********************** What about destructuring arrays?? **********************/
@@ -35,6 +37,13 @@ const cryptoCurrencies = ["bitcoin", "dash", "ethereum", "litecoin", "monero"]
 // const lite = cryptoCurrencies[3]
 
 // new way
+const [a = "another dollar", b, c, d, e, f = "money"] = cryptoCurrencies
+
+
+const apples = ["green", "yellow", "purple", ["rainbow", "blue"]]
+
+const [first, second, third, [rain, bloo]] = apples
+
 
 
 /********************** Function Example #1 **********************/
@@ -49,14 +58,17 @@ function getMovieTitles() {
 // const planetOfApes3 = moviesTitleArray[2]
 
 // new way
+const [planetOfApes1, , planetOfApes3] = getMovieTitles()
+
+
 
 
 /********************** Function Example #2 **********************/
-function shareContactInfo(employee) {
-    const name = employee.name
-    const position = employee.position
-    const cell = employee.cell
-    const work = employee.work
+function shareContactInfo({ name, position = "teacher", phones: { cell, work } }) {
+    // const name = employee.name
+    // const position = employee.position
+    // const cell = employee.phones.cell
+    // const work = employee.phones.work
 
     return `${name} is an ${position} who can be 
     reached by cell at ${cell} or by landline at ${work}`
@@ -64,11 +76,23 @@ function shareContactInfo(employee) {
 
 const e1 = {
     name: "Michelle",
-    position: "Instructor",
+    job: "Instructor",
     phones: {
         cell: "555-987-6543",
         work: "555-345-6789"
     }
 }
+const e2 = {
+    name: "Greg",
+    position: "Instructor",
+    phones: {
+        cell: "555-987-3455",
+        work: "555-345-6667"
+    }
+}
 
 // shareContactInfo(e1)
+
+
+
+// array of hashes
