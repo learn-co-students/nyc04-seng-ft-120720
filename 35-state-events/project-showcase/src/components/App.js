aa8d66754563dc02b441f7bb10ac44eaa08a35c3
ihollander <ianholla@gmail.com>
2021-02-23 09:23:17 -0500

    Before state and events

---
+++ 35-state-events/project-showcase/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import ProjectForm from "./ProjectForm";
+import ProjectList from "./ProjectList";
+// TODO: use to demo state/events
+// import ClickCounter from "./ClickCounter";
+
+// TODO: replace this with a fetch request (eventually)
+import projects from "../data/projects";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ProjectForm />
+      <ProjectList projects={projects} />
+    </div>
+  );
+}
+
+export default App;
---
+++ 35-state-events/project-showcase/src/components/ClickCounter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ClickCounter() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>There have been {0} clicks</h2>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default ClickCounter;
