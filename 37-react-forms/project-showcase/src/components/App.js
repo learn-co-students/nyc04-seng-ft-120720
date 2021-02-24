15daa6490245f3829cb61abeed704e5535eaa156
ihollander <ianholla@gmail.com>
2021-02-24 12:52:47 -0500

    Before forms

---
+++ 37-react-forms/project-showcase/src/components/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import ProjectForm from "./ProjectForm";
+import ProjectList from "./ProjectList";
+
+// TODO: replace this with a fetch request (eventually)
+import projectData from "../data/projects";
+
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [projects, setProjects] = useState(projectData);
+
+  function handleToggleDarkMode() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  return (
+    <div className={isDarkMode ? "App" : "App light"}>
+      <Header
+        name="Flatiron Projects"
+        isDarkMode={isDarkMode}
+        onToggleDarkMode={handleToggleDarkMode}
+      />
+      <ProjectForm />
+      <ProjectList projects={projects} />
+    </div>
+  );
+}
+
+export default App;
